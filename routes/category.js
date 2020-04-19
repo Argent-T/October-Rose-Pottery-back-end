e0e84b720fdd9bc12db925545d69d16763045500
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-04-19 19:38:08 -0400

    categories

---
+++ routes/category.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const router = express.Router();
+
+const {create} = require('../controllers/category');
+
+
+router.post('/category/create', create);
+
+
+module.exports = router;
