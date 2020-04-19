eb002f636743685118184f669731ff22dcb94e18
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-04-19 19:54:49 -0400

    product model

--- routes/category.js
+++ routes/category.js
@@ -2,9 +2,18 @@ const express = require ('express');
 const router = express.Router();
 
 const {create} = require('../controllers/category');
+const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
+const {userById} = require('../controllers/user');
 
+router.post(
+    '/category/create/:userId',
+    requireSignin, 
+    isAuth, 
+    isAdmin, 
+    create
+    );
 
-router.post('/category/create', create);
+router.param('userId', userById);
 
 
 module.exports = router;
