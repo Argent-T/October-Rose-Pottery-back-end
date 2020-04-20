178461f4b0253be499a31bbb76892ce2760c267a
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-04-19 20:10:59 -0400

    product controller

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require ('body-parser');
 const cookieParser = require ('cookie-parser');
 const expressValidator = require('express-validator');
 require('dotenv').config();
+
 //import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
 
 // app
 const app = express();
@@ -31,6 +33,7 @@ app.use(expressValidator());
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 
 const port = process.env.PORT || 8000;
