29b2759909263ef73e58eb7437b215bea5e79cf2
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-04-28 15:08:17 -0400

    photo

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const router = express.Router();
 
-const {create, productById, read, remove, update, list, listRelated, listCategories} = require('../controllers/product');
+const {create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo} = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
@@ -13,6 +13,8 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 router.get('/products', list)
 router.get('/products/related/:productId', listRelated)
 router.get('/products/categories', listCategories)
+router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId', photo);
 
 router.param('userId', userById);
 router.param("productId", productById);
