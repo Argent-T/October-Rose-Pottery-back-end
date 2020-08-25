ab041f2c48230c9c9d0803e64e17d1e079dabd38
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2020-08-25 10:16:02 -0400

    update order status

--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById, addOrderToUserHistory } = require('../controllers/user');
-const { create, listOrders, getStatusValues } = require('../controllers/order');
+const { create, listOrders, getStatusValues, orderById, updateOrderStatus } = require('../controllers/order');
 const { decreaseQuantity } = require('../controllers/product');
 
 router.post(
@@ -15,9 +15,32 @@ router.post(
    create
 );
 
-router.get('/order/list/:userId', requireSignin, isAuth, isAdmin, listOrders);
-router.get('/order/status-values/:userId', requireSignin, isAuth, isAdmin, getStatusValues);
+router.get(
+    '/order/list/:userId', 
+    requireSignin, 
+    isAuth, 
+    isAdmin, 
+    listOrders
+);
+
+router.get(
+    '/order/status-values/:userId',
+    requireSignin, 
+    isAuth, 
+    isAdmin,
+    getStatusValues
+);
+
+router.put(
+    '/order/:orderId/status/:userId',
+    requireSignin, 
+    isAuth, 
+    isAdmin,
+    updateOrderStatus
+);
+
 
 router.param('userId', userById);
+router.param('orderId', orderById);
 
 module.exports = router
